5a2d074c849f838aa094edd5b2b326a3d2de2125
zhulin-acad <zhulin.acad@gmail.com>
2025-06-07 12:21:17 +0800

    Update libraryFunctions.py

--- base/libraryFunctions.py
+++ base/libraryFunctions.py
@@ -42,34 +42,36 @@ factorial = factorial(5)  # 计算阶乘，用于组合优化
 
 comb = comb(5, 2)
 perm = perm(5, 2)
-print(comb)  # 10
-print(perm)  # 20
+# print(comb)  # 10
+# print(perm)  # 20
 
-from itertools import combinations, permutations
+from itertools import combinations, permutations, accumulate
 
 com = list(combinations([1, 2, 3], 2))
 per = list(permutations([1, 2, 3], 2))
 # print(list(com)  # 输出 [(1,2), (1,3), (2,3)]
 # print(list(per)  # 输出 [(1,2), (1,3), (2,1), (2,3), (3,1), (3,2)]
+acc = list(accumulate([1, 2, 3], initial = 0))
+# print(acc) # [0, 1, 3, 6]
 
 import heapq
 
 nums = [3, 1, 4, 1, 5, 9]
 heapq.heapify(nums)
-print(nums)  # 输出: [1, 1, 3, 4, 5, 9]
+# print(nums)  # 输出: [1, 1, 3, 4, 5, 9]
 
 heapq.heappush(nums, 2)
-print(nums)  # 输出：[1, 1, 2, 4, 5, 9, 3]
+# print(nums)  # 输出：[1, 1, 2, 4, 5, 9, 3]
 
 min_val = heapq.heappop(nums)
-print(min_val)  # 输出1
-print(nums)  # 输出: [1, 3, 2, 4, 5, 9]
+# print(min_val)  # 输出1
+# print(nums)  # 输出: [1, 3, 2, 4, 5, 9]
 
 new_min = heapq.heappushpop(nums, 0)  # 插入 0 并弹出最小元素
-print(new_min)  # 输出: 0
+# print(new_min)  # 输出: 0
 
 top3 = heapq.nlargest(3, nums)  # 获取前 3 大元素
-print(top3)  # 输出: [9, 5, 4]
+# print(top3)  # 输出: [9, 5, 4]
 
 # 实现最大堆
 nums = [3, 1, 4, 1, 5, 9]
@@ -78,7 +80,7 @@ heapq.heapify(max_heap)  # 转换为最小堆（实际为最大堆）
 
 # 获取最大值
 max_val = -heapq.heappop(max_heap)  # 取负还原
-print(max_val)  # 输出: 9
+# print(max_val)  # 输出: 9
 
 
 # 实现对排序
@@ -89,16 +91,16 @@ def heap_sort(arr):
 
 arr = [3, 1, 4, 1, 5, 9]
 sorted_arr = heap_sort(arr)
-print(sorted_arr)  # 输出: [1, 1, 3, 4, 5, 9]
+# print(sorted_arr)  # 输出: [1, 1, 3, 4, 5, 9]
 
 from datetime import date, timedelta
 d = date(2024, 6, 6)  # 创建一个日期对象（年, 月, 日）
-print(d.weekday()) # 3 表示星期四
-print(d.year, d.month, d.day)      # 输出：2024 6 6
+# print(d.weekday()) # 3 表示星期四
+# print(d.year, d.month, d.day)      # 输出：2024 6 6
 next_day = d + timedelta(days=1)
 prev_day = d - timedelta(days=1)
 d2 = date(2024, 7, 1)
-print(d2 - d) # 输出：25
+# print(d2 - d) # 输出：25
 today = date.today()  # 获取今天的日期
 
 import calendar
